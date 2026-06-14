a0d28093d0f86d910ddf69f8a955eb25468b2409
Heitor <heitorbolisw4@gmail.com>
2026-06-14 16:03:41 -0300

    add initial echo route

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/heitorbolisw4/api-students
+
+go 1.26.2
+
+require github.com/labstack/echo/v5 v5.2.0
+
+require golang.org/x/time v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,2 +1,30 @@
-main.go
+package main
 
+import (
+  "github.com/labstack/echo/v5"
+  "github.com/labstack/echo/v5/middleware"
+  "log/slog"
+  "net/http"
+)
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.RequestLogger()) // use the RequestLogger middleware with slog logger
+  e.Use(middleware.Recover())       // recover panics as errors for proper error handling
+
+  // Routes
+  e.GET("/students", getStudents)
+
+  // Start server
+  if err := e.Start(":8081"); err != nil {
+    slog.Error("failed to start server", "error", err)
+  }
+}
+
+// Handler
+func getStudents(c *echo.Context) error {
+  return c.String(http.StatusOK, "List of all students")
+}
